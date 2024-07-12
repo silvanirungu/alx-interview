871d34ca4ff596b5a1e87f517154a8bb90f3b1fd
silvanirungu <silvanirungu01@@gmail.com>
2024-07-12 08:06:12 +0000

    blackout

---
+++ 0x0A-primegame/0-prime_game_trial.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Prime game"""
+
+
+def primes(n):
+    """Returns a list of prime 1 and n inclusive
+    Args: n: upper range of prime
+    1: lower boundary"""
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
+
+
+def isWinner(x, nums):
+    """Checks if player x wins the game
+    Args: x: player's number, nums: upper limit range"""
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = Ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return "Maria"
+    elif Ben > Maria:
+        return "Ben"
+    return None
